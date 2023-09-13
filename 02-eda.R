f6c3dd87b7d7a9ca96ced9b5f8fe48c9f0f4ec5d
Nurhusniyah Syamimi binti Hassan <141210568+mimihassan@users.noreply.github.com>
2023-09-13 22:12:57 +0800

    Update 02-eda.R

--- 02-eda.R
+++ 02-eda.R
@@ -35,3 +35,23 @@ print(max_val_int)
 #   start seq         length parity  max_val seq_length max_value
 # <int> <list>       <dbl> <chr>     <dbl>      <int>     <dbl>
 #  1  9663 <dbl [185]>    185 Odd    27114424        185  27114424
+
+
+#3 Separate even and odd starting integers
+even_odd_stats <- collatz_df %>%
+  mutate(start_type = ifelse(start %% 2 == 0, "even", "odd"))
+
+print(even_odd_stats)
+
+# Calculate average length and standard deviation for even and odd starting integers
+even_odd_stats_summary <- even_odd_stats %>%
+  group_by(start_type = "even") %>%
+  summarize(avg_len = mean(seq_length), sd_len = sd(seq_length))
+
+# Extract the values for even and odd
+even_odd_avg_len <- even_odd_stats_summary$avg_len
+even_odd_sd_len <- even_odd_stats_summary$sd_len
+
+print(even_odd_stats_summary)
+print(even_odd_stats)
+print(even_odd_avg_len)
