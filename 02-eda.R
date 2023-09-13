219cb9326d62f4217d79d69acc7e3294fa51545f
Nurhusniyah Syamimi binti Hassan <141210568+mimihassan@users.noreply.github.com>
2023-09-13 15:25:04 +0800

    Update 02-eda.R

--- 02-eda.R
+++ 02-eda.R
@@ -9,17 +9,29 @@ top10longest <- collatz_df %>%
   head(10)
 
 print(collatz_df)
-#      A tibble: 10,000 × 6
-#        start seq        length parity max_val seq_length
-#     <int> <list>     <list> <list> <list>       <int>
-# 1      1 <chr [1]>  <NULL> <NULL> <NULL>           1
-# 2      2 <chr [2]>  <NULL> <NULL> <NULL>           2
-# 3      3 <chr [8]>  <NULL> <NULL> <NULL>           8
-# 4      4 <chr [3]>  <NULL> <NULL> <NULL>           3
-# 5      5 <chr [6]>  <NULL> <NULL> <NULL>           6
-# 6      6 <chr [9]>  <NULL> <NULL> <NULL>           9
-# 7      7 <chr [17]> <NULL> <NULL> <NULL>          17
-# 8      8 <chr [4]>  <NULL> <NULL> <NULL>           4
-# 9      9 <chr [20]> <NULL> <NULL> <NULL>          20
-# 10    10 <chr [7]>  <NULL> <NULL> <NULL>           7
 
+#  A tibble: 10,000 × 6
+#        start seq        length parity max_val seq_length
+#      <int> <list>      <dbl> <chr>    <dbl>      <int>
+#  1     1 <dbl [1]>       1 Odd          1          1
+#  2     2 <dbl [2]>       2 Even         2          2
+#  3     3 <dbl [8]>       8 Odd         16          8
+#  4     4 <dbl [3]>       3 Even         4          3
+#  5     5 <dbl [6]>       6 Odd         16          6
+#  6     6 <dbl [9]>       9 Even        16          9
+#  7     7 <dbl [17]>     17 Odd         52         17
+#  8     8 <dbl [4]>       4 Even         8          4
+#  9     9 <dbl [20]>     20 Odd         52         20
+#  10    10 <dbl [7]>       7 Even        16          7
+
+#2 Find the row where the maximum value in the sequence is achieved
+max_val_int <- collatz_df %>%
+  mutate(max_value = sapply(seq, max)) %>%
+  filter(max_value == max(max_value))
+
+print(max_val_int)
+
+# A tibble: 1 × 7
+#   start seq         length parity  max_val seq_length max_value
+# <int> <list>       <dbl> <chr>     <dbl>      <int>     <dbl>
+#  1  9663 <dbl [185]>    185 Odd    27114424        185  27114424
