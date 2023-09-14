1378427ff0930f91abb43187e355715055805fcf
Nurhusniyah Syamimi binti Hassan <19b9048@ubd.edu.bn>
2023-09-14 22:44:33 +0800

    Update 02-eda.R

--- 02-eda.R
+++ 02-eda.R
@@ -5,28 +5,7 @@ collatz_df <- collatz_df %>%
 
 #1 Find top 10 longest sequences
 top10longest <- collatz_df %>%
-  arrange(desc(seq_length)) %>%
+  arrange(collatz_df = seq, desc(seq_length)) %>%
   head(10)
 
-#2 Find the row where the maximum value in the sequence is achieved
-max_val_int <- collatz_df %>%
-  mutate(max_value = sapply(seq, max)) %>%
-  filter(max_value == max(max_value))
-
-
-#3 Separate even and odd starting integers
-even_odd_stats <- collatz_df %>%
-  mutate(start_type = ifelse(start %% 2 == 0, "even", "odd"))
-
-# Calculate average length and standard deviation for even and odd starting integers
-even_odd_stats_summary <- even_odd_stats %>%
-  group_by(start_type = "even") %>%
-  summarize(avg_len = mean(seq_length), sd_len = sd(seq_length))
-
-# Extract the values for even and odd
-even_odd_avg_len <- even_odd_stats_summary$avg_len
-even_odd_sd_len <- even_odd_stats_summary$sd_len
-
-print(even_odd_stats_summary)
-print(even_odd_stats)
-print(even_odd_avg_len)
+print(top10longest)
