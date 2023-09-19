eade205ceb79a503432e323d96fa98984c378319
Nurhusniyah Syamimi binti Hassan <19b9048@ubd.edu.bn>
2023-09-19 12:22:35 +0800

    Update 02-eda.R

--- 02-eda.R
+++ 02-eda.R
@@ -8,4 +8,24 @@ top10longest <- collatz_df %>%
   arrange(desc(length)) %>%
   top_n(10, seq_length)
 
+#2. Find out which starting integer produces a sequence that reaches the highest maximum value [max_val_int]
+
+max_val_int <- collatz_df %>%
+  filter(max_val == max(max_val)) %>%
+  select(start, max_val)
+
+
+#3. What is the average length and standard deviation of the sequence for even starting integers compared to odd ones? [even_odd_avg_len and even_odd_sd_len]
+
+even_odd_avg_len <- collatz_df %>%
+  group_by(parity) %>%
+  summarize(avg_length = mean(length, na.rm = TRUE), sd_length = sd(length, na.rm = TRUE))
+
+
+
+saveRDS(top10longest, file = "top10longest.rds")
+saveRDS(max_val_int, file = "max_val_int.rds")
+saveRDS(even_odd_avg_len, file = "even_odd_avg_len.rds")
+
+
 
