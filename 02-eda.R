52e091faa9d589c74784f7697e26c29e0210261b
Nurhusniyah Syamimi binti Hassan <19b9048@ubd.edu.bn>
2023-09-19 12:17:17 +0800

    Update 02-eda.R

--- 02-eda.R
+++ 02-eda.R
@@ -1,9 +1,11 @@
 library(tidyverse)
 
+collatz_df <- collatz_df %>%
+  mutate(seq_length = sapply(seq, length))
+
 #1. Find the top 10 starting integers that produce the longest sequences [top10longest]
 top10longest <- collatz_df %>%
   arrange(desc(length)) %>%
   top_n(10, seq_length)
-
 
 
