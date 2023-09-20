234a7c30d8c58718b7a4920b47589e31f94a549e
Amira Barizah <128048324+amirabarizah@users.noreply.github.com>
2023-09-20 22:56:27 +0800

    Update 02-eda.R

--- 02-eda.R
+++ 02-eda.R
@@ -16,41 +16,6 @@ saveRDS(top10longest, file = "top10longest.rds")
 
 #2. Find out which starting integer produces a sequence that reaches the highest maximum value [max_val_int]
 collatz_df <- collatz_df %>%
-   mutate(max_val = sapply(seq, max))
-
- max_val_int <- collatz_df %>%
-   filter(max_val == max(collatz_df$max_val)) %>%
-   select(start)
-
-saveRDS(max_val_int, file = "max_val_int.rds")
-
-#3. What is the average length and standard deviation of the sequence for even starting integers compared to odd ones? [even_odd_avg_len and even_odd_sd_len]
-even_odd_avg_len <- collatz_df %>%
-  group_by(parity) %>%
-  summarize(even_odd_avg_len = mean(length))
-
-even_odd_sd_len <- collatz_df %>%
-  group_by(parity) %>%
-  summarize(even_odd_sd_len = sd(length))
-
-library(tidyverse)
-
-#1. Find the top 10 starting integers that produce the longest sequences [top10longest]
-collatz_df <- collatz_df %>%
-  mutate(seq_length = sapply(seq, length))
-
-top10longest <- collatz_df %>%
-  arrange(desc(seq_length)) %>%
-  head(10)
-
-top10longest <- top10longest %>%
-  select(start) %>%
-  pivot_wider(names_from = start, values_from = start)
-
-saveRDS(top10longest, file = "top10longest.rds")
-
-#2. Find out which starting integer produces a sequence that reaches the highest maximum value [max_val_int]
-collatz_df <- collatz_df %>%
   mutate(max_val = sapply(seq, max))
 
 max_val_int <- collatz_df %>%
