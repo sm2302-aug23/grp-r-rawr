8ce733f9786035d9b98ebd85c9088e7a641190c1
amirabarizah <d.amirabarizah@gmail.com>
2023-09-20 13:49:15 +0800

    Create 03-backtracking.R

---
+++ 03-backtracking.R
@@ -0,0 +1,24 @@
+library(tidyverse)
+
+#1: Filter for backtracking sequences
+backtracks_df <- collatz_df %>%
+  filter(
+    seq_along(seq) > 2,  
+    any(seq < start),   
+    any(seq > start)    
+  )
+
+#2: Find the mode backtrack (most frequent occuring number of )
+backtrack_counts <- backtracks_df %>%
+  mutate(
+    backtrack_count = sum(seq < start),
+    max_after_backtrack = sapply(seq, function(x) max(x[x > start]))
+  )
+
+mode_backtrack <- as.integer(names(sort(table(backtrack_counts$backtrack_count), decreasing = TRUE)[1]))
+
+#3: Calculate the maximum value reached after the first backtrack
+max_after_backtrack <- max(backtrack_counts$max_after_backtrack)
+
+#4:Calculate the frequency counts for even and odd backtracking integers
+
