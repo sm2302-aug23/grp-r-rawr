668b0a653df3b90b2118ed1fc12259e8d6a40e48
Amira Barizah <128048324+amirabarizah@users.noreply.github.com>
2023-09-20 15:33:56 +0800

    Update 03-backtracking.R

--- 03-backtracking.R
+++ 03-backtracking.R
@@ -9,15 +9,14 @@ mode_backtrack <- backtracks_df %>%
   mutate(backtrack_df = sapply(seq, function(x) sum(x > x[1]))) %>%
   summarise(mode_backtrack = as.numeric(names(sort(table(backtrack_df), decreasing = TRUE)[1])))
 
-mode_backtrack <- as.numeric(names(sort(table(mode_backtrack$mode_backtrack), decreasing = TRUE)[1]))
-
+mode_backtrack <- as.integer(mode_backtrack)
 
 #3: Calculate the maximum value reached after the first backtrack
 max_after_backtrack <- backtracks_df %>%
   mutate(max_after_backtrack = sapply(seq, function(x) max(x[x > x[1]]))) %>%
   summarise(max_after_backtrack = max(max_after_backtrack))
 
-max_after_backtrack <- as.numeric(names(sort(table(max_after_backtrack$max_after_backtrack), decreasing = TRUE)[1]))
+max_after_backtrack <- as.integer(max_after_backtrack)
 
 #4: Calculate the frequency counts for even and odd backtracking integers
 even_odd_backtrack <- backtracks_df %>%
