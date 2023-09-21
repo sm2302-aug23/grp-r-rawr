2123b65f4b8556eb02aa4b7c9211cae430aee574
Amira Barizah <128048324+amirabarizah@users.noreply.github.com>
2023-09-22 03:54:02 +0800

    Update 02-eda.R

--- 02-eda.R
+++ 02-eda.R
@@ -28,19 +28,13 @@ saveRDS(max_val_int, file = "max_val_int.rds")
 even_odd_avg_len <- collatz_df %>%
   group_by(parity) %>%
   summarize(even_odd_avg_len = mean(seq_length)) %>%
-  select(even_odd_avg_len) %>%
-  unlist()
-
-even_odd_avg_len[2] / even_odd_avg_len[1]
+  select(even_odd_avg_len)
 
 saveRDS(even_odd_avg_len, file = "even_odd_avg_len.rds")
 
 even_odd_sd_len <- collatz_df %>%
   group_by(parity) %>%
   summarize(even_odd_sd_len = sd(seq_length)) %>%
-  select(even_odd_sd_len) %>%
-  unlist()
-
-even_odd_sd_len[2] / even_odd_sd_len[1]
+  select(even_odd_sd_len) 
 
 saveRDS(even_odd_sd_len, file = "even_odd_sd_len.rds")
