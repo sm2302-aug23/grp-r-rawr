2133c78bcb650b5fbf2ff3dd027715d13310e3aa
Amira Barizah <128048324+amirabarizah@users.noreply.github.com>
2023-09-22 01:51:45 +0800

    Update 02-eda.R

--- 02-eda.R
+++ 02-eda.R
@@ -1,54 +1,53 @@
-
-#1. Find the top 10 starting integers that produce the longest sequences [top10longest]
-collatz_seq <- function(n) {
-  sequence <- c(n)
-  while (n != 1) {
-    if (n %% 2 == 0) {
-      n <- n / 2
-    } else {
-      n <- 3*n + 1
-    }
-    seq <- c(sequence, n)
-  }
-  return(sequence)
-}
-
-start_int <- 1:100
-sequences <- lapply(start_int, collatz_seq)
-
 library(tidyverse)
 
-collatz_df <- tibble(start_int = start_int, sequence = sequences)
+#1. Find the top 10 starting integers that produce the longest sequences [top10longest]
+collatz_df <- collatz_df %>%
+  mutate(seq_length = sapply(seq, length))
 
 top10longest <- collatz_df %>%
-  mutate(sequence_length = map_int(sequence, length)) %%
-  arrange(desc(sequence_length)) %>%
-  slice_head(n =10)
+  arrange(desc(seq_length)) %>%
+  head(10)
+
+top10longest <- top10longest %>%
+  select(start) %>%
+  pivot_wider(names_from = start, values_from = start)
+
+top10longest <- unlist(top10longest)
 
 saveRDS(top10longest, file = "top10longest.rds")
 
 #2. Find out which starting integer produces a sequence that reaches the highest maximum value [max_val_int]
+collatz_df <- collatz_df %>%
+  mutate(max_val = sapply(seq, max))
+
 max_val_int <- collatz_df %>%
-  mutate(max_val = map_dbl(sequence, max)) %>%
-  filter(max_val == max(max_val)) %>%
-  
+  arrange(desc(max_val)) %>%
+  head(1) %>%
+  select(start) %>%
+  pivot_wider(names_from = start, values_from = start)
+
 saveRDS(max_val_int, file = "max_val_int.rds")
 
 #3. What is the average length and standard deviation of the sequence for even starting integers compared to odd ones? [even_odd_avg_len and even_odd_sd_len]
 even_odd_avg_len <- collatz_df %>%
-  mutate(is_even = start_int %% 2 == 0) %>%
-  group_by(is_even) %>%
-  summarize(avg_length = mean(map_dbl(sequence, ~length(.))))
+  mutate(seq_length = as.numeric(seq_length)) %>%
+  group_by(parity) %>%
+  summarize(even_odd_avg_len = mean(seq_length, na.rm = TRUE))
 
 even_odd_sd_len <- collatz_df %>%
-  mutate(is_even = start_int %% 2 == 0) %>%
-  group_by(is_even) %>%
-  summarize(sd_length = sd(map_dbl(sequence, ~length(.))))
+  mutate(seq_length = as.numeric(seq_length)) %>%
+  group_by(parity) %>%
+  summarize(even_odd_sd_len = sd(seq_length, na.rm = TRUE))
 
+even_odd_summary <- collatz_df %>%
+  group_by(parity) %>%
+  summarize(
+    even_odd_avg_len = mean(seq_length),
+    even_odd_sd_len = sd(seq_length)
+  )
 
-saveRDS(even_odd_avg_len, file = "even_odd_avg_len.rds")
-
-saveRDS(even_odd_sd_len, file = "even_odd_sd_len.rds")
+saveRDS(even_odd_summary, file = "even_odd_summary.rds")
 
 combined_df <- bind_cols(max_val_int, top10longest)
 
+str(collatz_df)
