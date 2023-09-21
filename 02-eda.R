13f9fe58ea595f7aedd1eb81c02a5639d79c62a5
amirabarizah <d.amirabarizah@gmail.com>
2023-09-21 14:46:26 +0800

    Update

--- 02-eda.R
+++ 02-eda.R
@@ -1,31 +1,51 @@
 library(tidyverse)
 
+library(tidyverse)
+
+#1. Find the top 10 starting integers that produce the longest sequences [top10longest]
 collatz_df <- collatz_df %>%
   mutate(seq_length = sapply(seq, length))
 
-#1. Find the top 10 starting integers that produce the longest sequences [top10longest]
 top10longest <- collatz_df %>%
   arrange(desc(seq_length)) %>%
   head(10)
 
+top10longest <- top10longest %>%
+  select(start) %>%
+  pivot_wider(names_from = start, values_from = start)
+
+saveRDS(top10longest, file = "top10longest.rds")
+
 #2. Find out which starting integer produces a sequence that reaches the highest maximum value [max_val_int]
+collatz_df <- collatz_df %>%
+  mutate(max_val = sapply(seq, max))
+
 max_val_int <- collatz_df %>%
-  mutate(max_val = sapply(seq, max)) %>%
-  filter(max_val == max(max_val)) 
+  arrange(desc(max_val)) %>%
+  head(1) %>%
+  select(start) %>%
+  pivot_wider(names_from = start, values_from = start)
+
+saveRDS(max_val_int, file = "max_val_int.rds")
+
 
 #3. What is the average length and standard deviation of the sequence for even starting integers compared to odd ones? [even_odd_avg_len and even_odd_sd_len]
-even_odd_stats <- collatz_df %>%
-  mutate(start_type = ifelse(start %% 2 == 0, "even", "odd"))
+even_odd_avg_len <- collatz_df %>%
+  mutate(length = as.numeric(length)) %>%
+  group_by(parity) %>%
+  summarize(even_odd_avg_len = mean(length))
+
+even_odd_sd_len <- collatz_df %>%
+  mutate(length = as.numeric(length)) %>%
+  group_by(parity) %>%
+  summarize(even_odd_sd_len = sd(length))
 
 even_odd_summary <- collatz_df %>%
-  mutate(is_even = start %% 2 == 0) %>%
-  group_by(is_even) %>%
+  group_by(parity) %>%
   summarize(
     even_odd_avg_len = mean(seq_length),
     even_odd_sd_len = sd(seq_length)
   )
 
-saveRDS(top10longest, file = "top10longest.rds")
-saveRDS(max_val_int, file = "max_val_int.rds")
 saveRDS(even_odd_summary, file = "even_odd_summary.rds")
 
--- 03-backtracking.R
+++ 03-backtracking.R
@@ -6,13 +6,17 @@ backtracks_df <- collatz_df %>%
 
 #2: Find the mode backtrack
 mode_backtrack <- backtracks_df %>%
-  mutate(backtrack_count = sapply(seq, function(x) sum(x > x[1]))) %>%
-  summarise(mode_backtrack = as.numeric(names(sort(table(backtrack_count), decreasing = TRUE)[1])))
+  mutate(backtrack_df = sapply(seq, function(x) sum(x > x[1]))) %>%
+  summarise(mode_backtrack = as.numeric(names(sort(table(backtrack_df), decreasing = TRUE)[1])))
+
+mode_backtrack <- as.integer(mode_backtrack)
 
 #3: Calculate the maximum value reached after the first backtrack
 max_after_backtrack <- backtracks_df %>%
   mutate(max_after_backtrack = sapply(seq, function(x) max(x[x > x[1]]))) %>%
   summarise(max_after_backtrack = max(max_after_backtrack))
+
+max_after_backtrack <- as.integer(max_after_backtrack)
 
 #4: Calculate the frequency counts for even and odd backtracking integers
 even_odd_backtrack <- backtracks_df %>%
@@ -21,10 +25,5 @@ even_odd_backtrack <- backtracks_df %>%
     Odd = n() - sum(start %% 2 == 0)
   )
 
-print("Most frequently occurring backtrack count:")
-print(mode_backtrack$mode_backtrack)
-print("Maximum value after the first backtrack:")
-print(max_after_backtrack$max_after_backtrack)
-print("Frequency of backtracking among even and odd starting integers:")
-print("Even:", even_odd_backtrack$Even)
-print("Odd:", even_odd_backtrack$Odd)
+even_odd_backtrack$Even
+even_odd_backtrack$Odd
