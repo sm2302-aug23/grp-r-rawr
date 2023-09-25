89d37c70c457ce90e6d69aa715e60ac8e0728b22
ninazuhairi <127283289+ninazuhairi@users.noreply.github.com>
2023-09-26 06:34:38 +0800

    q3

--- 03-backtracking.R
+++ 03-backtracking.R
@@ -1,21 +1,15 @@
 library(tidyverse)
-
+library(magrittr)
 
 #1: Filter for backtracking sequences
 backtracks_df <- collatz_df %>%
   filter(sapply(seq, function(x) any(x > x[1])))
-
+  
 #2: Find the mode backtrack
 mode_backtrack <- backtracks_df %>%
   mutate(backtrack_count = sapply(seq, function(x) sum(x > x[1]))) %>%
-  
-                              
-mode_backtrack <- mode_backtrack %>%                                  
-  pivot_wider(names_from = start, values_from = backtracks_df$backtrack_count) %>%
   summarise(mode_backtrack = as.numeric(names(sort(table(backtrack_count), decreasing = TRUE)[1]))) %>%
   unlist()
-
-
 
 #3: Calculate the maximum value reached after the first backtrack
 max_after_backtrack <- backtracks_df %>%
