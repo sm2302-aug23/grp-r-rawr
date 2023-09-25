993ef9feead1cf36da9b339291faa77cdf86f0ad
hani-hive <128375120+hani-hive@users.noreply.github.com>
2023-09-25 22:16:01 +0900

    Update 06-creative.R

--- 06-creative.R
+++ 06-creative.R
@@ -1,1 +1,31 @@
+## Sequence Length Heatmap
+
+library(tidyverse)
+
+# Function to generate Collatz sequences
+gen_collatz <- function(n) {
+  sequence <- numeric(0)
+  while (n != 1) {
+    sequence <- c(sequence, n)
+    if (n %% 2 == 0) {
+      n <- n / 2
+    } else {
+      n <- 3 * n + 1
+    }
+  }
+  return(c(sequence, 1))
+}
+
+# Create a dataframe with starting integers and sequence lengths
+start_integers <- 1:10000
+sequence_lengths <- sapply(start_integers, function(x) length(gen_collatz(x)))
+df <- data.frame(Starting_Integer = start_integers, Sequence_Length = sequence_lengths)
+
+# Create the heatmap
+ggplot(df, aes(x = Starting_Integer, y = Sequence_Length, fill = Sequence_Length)) +
+  geom_tile() +
+  scale_fill_gradient(low = "white", high = "blue") +
+  labs(x = "Starting Integer", y = "Sequence Length to Reach 1") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
