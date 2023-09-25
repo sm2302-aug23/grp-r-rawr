75deb0a47940d49f1c9d9f81a5efd307e68ae7ec
Nurhusniyah Syamimi binti Hassan <19b9048@ubd.edu.bn>
2023-09-25 20:24:14 +0800

    Update 05-open.R

--- 05-open.R
+++ 05-open.R
@@ -1,6 +1,6 @@
 ## Open-ended exploration
 
-#1 What is the most frequent integer that appears in all the sequences combined, excluding the number 1?
+# What is the most frequent integer that appears in all the sequences combined, excluding the number 1?
 
 library(tidyverse)
 
@@ -20,12 +20,4 @@ most_frequent_integer <- as.numeric(names(which.max(frequency_counts)))
 print(paste("The most frequent integer (excluding 1) is:", most_frequent_integer))
 
 #[1] "The most frequent integer (excluding 1) is: 2"
-
-
-#2 Is there any correlation between the starting integers and the number of odd or even numbers in their sequence?
-
-
-
-
-#3 What are the most common “stopping times,” i.e., the number of steps it takes for a sequence to first go below its starting integer?
 
