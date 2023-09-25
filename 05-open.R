135c86b99f149a5b81bfebc3a4ce374113b3a469
Nurhusniyah Syamimi binti Hassan <19b9048@ubd.edu.bn>
2023-09-25 14:39:24 +0800

    Update 05-open.R

--- 05-open.R
+++ 05-open.R
@@ -1,1 +1,5 @@
+## Open-ended exploration
+# What is the most frequent integer that appears in all the sequences combined, excluding the number 1?
+# Is there any correlation between the starting integers and the number of odd or even numbers in their sequence?
+# What are the most common “stopping times,” i.e., the number of steps it takes for a sequence to first go below its starting integer?
 
