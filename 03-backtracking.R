e5ba1023ff16de43cb028ab7977bea5e2d355dc4
ninazuhairi <127283289+ninazuhairi@users.noreply.github.com>
2023-09-26 09:42:21 +0800

    Update 03-backtracking.R

--- 03-backtracking.R
+++ 03-backtracking.R
@@ -7,9 +7,9 @@ backtracks_df <- collatz_df %>%
   
 #2: Find the mode backtrack
 mode_backtrack <- backtracks_df %>%
-  count(seq) %>%
-  filter(n == max(n)) %>%
-  pull(seq)
+  mutate(backtrack_count = sapply(seq, function(x) sum(x > x[1]))) %>%
+  summarise(mode_backtrack = as.numeric(names(sort(table(backtrack_count), decreasing = TRUE)[1]))) %>%
+  unlist()
 
 #3: Calculate the maximum value reached after the first backtrack
 max_after_backtrack <- backtracks_df %>%
