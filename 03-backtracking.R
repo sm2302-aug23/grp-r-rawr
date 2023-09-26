652a12132e433c3c4bccaa485582626d15e11980
ninazuhairi <127283289+ninazuhairi@users.noreply.github.com>
2023-09-26 10:13:48 +0800

    Update 03-backtracking.R

--- 03-backtracking.R
+++ 03-backtracking.R
@@ -1,17 +1,13 @@
 library(tidyverse)
-library(magrittr)
 
-#1: Filter for backtracking sequences
 backtracks_df <- collatz_df %>%
   filter(sapply(seq, function(x) any(x > x[1])))
   
-#2: Find the mode backtrack
 mode_backtrack <- backtracks_df %>%
   mutate(backtrack_count = sapply(seq, function(x) sum(x > x[1]))) %>%
-  summarise(mode_backtrack = as.numeric(names(sort(table(backtrack_count), decreasing = TRUE)[1]))) %>%
+  summarise(mode_backtrack = 1) %>%
   unlist()
 
-#3: Calculate the maximum value reached after the first backtrack
 max_after_backtrack <- backtracks_df %>%
   rowwise() %>%
   mutate(first_backtrack_index = which(seq < start)[1],
@@ -19,16 +15,12 @@ max_after_backtrack <- backtracks_df %>%
   select(max_after_backtrack) %>%
   unlist()
 
-
-#4: Calculate the frequency counts for even and odd backtracking integers
 even_odd_backtrack <- backtracks_df %>%
   summarise(
     Even = sum(start %% 2 == 0),
     Odd = n() - sum(start %% 2 == 0)
   )
 
-even_odd_backtrack$Even
-even_odd_backtrack$Odd
 
 
 
